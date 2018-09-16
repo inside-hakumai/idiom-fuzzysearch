7d7cd3fc7d930c0a32047ed54bde1ab5d5bb34d7
Hakumai <inside.rice164@gmail.com>
2018-09-16 21:37:31 +0900

    デプロイ用の設定を追加

--- web/webpack.prod.js
+++ web/webpack.prod.js
@@ -1,8 +1,19 @@
 const merge = require('webpack-merge');
 const common = require('./webpack.common');
 
-const config = merge(common, {
+const clientConfig = merge(common, {
   mode: 'production'
 });
 
-module.exports = config;
+const serverConfig = merge(clientConfig, {
+  target: 'node',
+  entry: {
+    'server-app': './app.ts',
+  },
+  output: {
+    path: __dirname,
+    filename: 'app.js'
+  }
+});
+
+module.exports = [clientConfig, serverConfig];
